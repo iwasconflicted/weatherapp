97d709e675ca8e96aa373c1dee2c7bda34ca39c9
Saroeut Sim <saroeuts123@gmail.com>
2024-03-27 15:56:48 -0700

    added functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,7 +10,7 @@ let searchBar = document.getElementById("searchBar").addEventListener("click", f
 })
  
 let saveCityBtn = document.getElementById("saveCityBtn").addEventListener("click", function(){
-
+    document.getElementById("saveCityBtn").src = "./assets/heart.png";
 });
 
 
@@ -27,5 +27,32 @@ let dayFourTemp = document.getElementById("dayFourTemp");
 let dayFiveTemp = document.getElementById("dayFiveTemp");
 
 
+async function getLocation(cityName){
+    let weatherApi = await fetch("http://api.openweathermap.org/geo/1.0/direct?q="+ cityName +"&limit=2&appid=206b289f9429e5805345fdcf42f2d083").then((Response) => Response.json());
+    lat = weatherApi["0"].lat.toString();
+    lon = weatherApi["0"].lon.toString();
+    currentLocation.innerText = weatherApi["0"].name + ", " + weatherApi["0"].state;
+    getCurrentWeather(lat, lon);
+    getFiveDayForecast(lat, lon);
+    console.log(weatherApi);
+};
 
+async function getCurrentWeather(lat, lon){
+    let weatherApi = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=206b289f9429e5805345fdcf42f2d083&units=imperial").then((Response) => Response.json());
+    currentTemp.innerText = weatherApi.main.temp;
+    currentTemp.innerText = Math.floor(currentTemp.innerText)+ "°";
+    highTemp.innerText = Math.floor(weatherApi.main.temp_max) + "°";
+    lowTemp.innerText = Math.floor(weatherApi.main.temp_min)  + "°";
+    console.log(weatherApi);
+};
 
+//Five day forecast API
+async function getFiveDayForecast(lat, lon){
+    let weatherApi = await fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=206b289f9429e5805345fdcf42f2d083&units=imperial").then((Response) => Response.json());
+    dayOneTemp.innerText = Math.floor(weatherApi.list["3"].main.temp) + "°";
+    dayTwoTemp.innerText = Math.floor(weatherApi.list["6"].main.temp) + "°";
+    dayThreeTemp.innerText = Math.floor(weatherApi.list["27"].main.temp) + "°";
+    dayFourTemp.innerText = Math.floor(weatherApi.list["33"].main.temp) + "°";
+    dayFiveTemp.innerText = Math.floor(weatherApi.list["38"].main.temp) + "°";
+    console.log(weatherApi);
+};
